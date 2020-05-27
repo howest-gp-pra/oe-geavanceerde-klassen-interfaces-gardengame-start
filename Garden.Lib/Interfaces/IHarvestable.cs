c43af9c84c497a4702146cb26e16bb300a9c432b
maximlesy <maximlesy@gmail.com>
2020-05-27 14:02:58 +0200

    Initial commit

---
+++ Garden.Lib/Interfaces/IHarvestable.cs
@@ -0,0 +1,15 @@
+﻿using Garden.Lib.Classes.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden.Lib.Interfaces
+{
+    public interface IHarvestable
+    {
+        int HarvestSize { get; }
+        Harvest GetHarvest();
+    }
+}
